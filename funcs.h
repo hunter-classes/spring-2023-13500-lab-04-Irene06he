ad8e42b02678e312f250be0cb951f3255d4f5b2f
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-24 04:58:52 -0600

    add once

---
+++ funcs.h
@@ -0,0 +1,12 @@
+//Name : Irene He He
+//GitHub Name : Irene06he
+
+#pragma once
+
+std::string box(int width, int height);
+std::string checkerboard(int width, int height);
+std::string cross (int size);
+std::string lower(int length);
+std::string upper(int length);
+std::string trapezoid(int width, int height);
+std::string checkerboard3x3(int width, int height);
